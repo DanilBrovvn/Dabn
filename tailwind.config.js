4152b49405533ec7bf21f74846e412424e9e23dc
DanilBrovvn <danil.brovvn@gmail.com>
2025-03-07 03:55:59 +0300

    111111

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: {
+          50: '#fdf2f8', // Нежно-розовый
+          900: '#831843', // Темно-розовый
+        },
+        lavender: {
+          50: '#f5f3ff', // Лавандовый
+          900: '#4c1d95', // Темно-лавандовый
+        },
+        mint: {
+          50: '#f0fdfa', // Мятный
+          900: '#134e4a', // Темно-мятный
+        },
+        peach: {
+          50: '#fff7ed', // Персиковый
+          900: '#7c2d12', // Темно-персиковый
+        },
+      },
+    },
   },
   plugins: [],
 };
